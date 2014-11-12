3875c659e141f9ffe76b69c0224b0c0b6783d611
tommyfok <154274174@qq.com>
2014-11-12 19:05:27 +0800

    fix: minor bug.

--- easy-notification.js
+++ easy-notification.js
@@ -1,7 +1,7 @@
 function EasyNotify(title, content, iconUrl, ifFailedCallback) {
   var Notification = window.Notification || navigator.webkitNotifications,
       content = content || '',
-      iconUrl = iconUrl || 'https://github.com/favicon.ico',
+      iconUrl = iconUrl || '//' + location.host + '/favicon.ico',
       ifFailedCallback = typeof ifFailedCallback === 'function' ? ifFailedCallback : function () {};
 
   if (!Notification) {
@@ -10,14 +10,18 @@ function EasyNotify(title, content, iconUrl, ifFailedCallback) {
 
   else if (Notification.permission === 'granted') {
     var notification = new Notification(title, {icon: iconUrl, body: content});
-    notification.onclick = window.focus;
+    notification.onclick = function () {
+      window.focus();
+    };
   }
 
   else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       if (permission === 'granted') {
         var notification = new Notification(title, {icon: iconUrl, body: content});
-        notification.onclick = window.focus;
+        notification.onclick = function () {
+          window.focus();
+        };
       } else {
         window.Notification = function () {};
       }
